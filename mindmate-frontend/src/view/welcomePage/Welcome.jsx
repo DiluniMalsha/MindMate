49a9d42e5424e1ae8ecfb47e1cd49450290e98a8
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-06-23 18:42:14 +0530

    link get stranded btn to login form

--- mindmate-frontend/src/view/welcomePage/Welcome.jsx
+++ mindmate-frontend/src/view/welcomePage/Welcome.jsx
@@ -1,5 +1,6 @@
 import "./Welcome.css";
 import logo from "../../assets/logo/Logo.png";
+<<<<<<< Updated upstream
 import robot from "../../assets/robotPicture/robot.png"
 import CustomButton from "../../components/button/CustomButton";
 
@@ -15,6 +16,34 @@ const Welcome = (props) => {
             </CustomButton>
         </div>
     );
+=======
+import robot from "../../assets/robotPicture/robot.png";
+import CustomButton from "../../components/button/CustomButton";
+import { Link } from "react-router-dom";
+
+const Welcome = (props) => {
+  return (
+    <div className="welcome-main">
+      <img src={logo} alt="MindMate" className="maidMate-logo" />
+      <p className="welcome">Welcome Back</p>
+      <img src={robot} alt="Robot" className="robot" />
+      <br />
+      <Link to="/signIn">
+        <CustomButton
+          type="button"
+          variant="light"
+          radius="20"
+          size="sm"
+          className="mt-5"
+          fontSize="20"
+          width="200"
+        >
+          Get Started
+        </CustomButton>
+      </Link>
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Welcome;
