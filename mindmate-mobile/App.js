93edbc98231f588dc62998f6bcfb8a32cc7b135c
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-06-27 16:19:32 +0530

    Created WelcomeScreen and LoginScreen. Added fonts

--- mindmate-mobile/App.js
+++ mindmate-mobile/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded] = useFonts({
+    LatoBlack: require("./app/assets/fonts/Lato/Lato-Black.ttf"),
+    LatoBold: require("./app/assets/fonts/Lato/Lato-Bold.ttf"),
+    LatoLight: require("./app/assets/fonts/Lato/Lato-Light.ttf"),
+    LatoRegular: require("./app/assets/fonts/Lato/Lato-Regular.ttf"),
+    LatoThin: require("./app/assets/fonts/Lato/Lato-Thin.ttf"),
+  });
+  if (!loaded) {
+    return null;
+  }
+  return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mindmate-mobile/app/config/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#171742",
+  white: "#fff",
+  ash: "#414141",
+};
