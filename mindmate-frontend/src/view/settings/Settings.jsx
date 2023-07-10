b150359f35fd860b85c1fbdb0018f569ce58fd29
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-07-10 22:00:31 +0530

    updated settings page

--- mindmate-frontend/src/view/settings/Settings.jsx
+++ mindmate-frontend/src/view/settings/Settings.jsx
@@ -25,7 +25,7 @@ function Settings() {
         <section className="vh-100">
             <div className="container-fluid h-custom">
                 <div className="row d-flex justify-content-center align-items-center h-100 m-0">
-                    <div className="col-md-9 col-lg-6 col-xl-5 ">
+                    <div className="col-md-9   ">
                         <MDBContainer className="main-section">
                             <h1 className="setting-heading">Settings</h1>
 
