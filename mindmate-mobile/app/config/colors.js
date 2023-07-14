c836ad2f1ba08ecdf026d4acd6b3d0b07bd5eedd
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-07-14 21:11:49 +0530

    Updated the primary color and added new colors

--- mindmate-mobile/app/config/colors.js
+++ mindmate-mobile/app/config/colors.js
@@ -1,8 +1,9 @@
 export default {
-  primary: "#171742",
+  primary: "#1e5d88",
   white: "#fff",
   ash: "#414141",
   black: "#000000",
   red: "#CD1818",
   yellow: "#F8A910",
+  blue: "#1f65ff",
 };
