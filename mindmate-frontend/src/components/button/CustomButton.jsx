5309c2661754f854d5bbfd8ede8d83e4ab9add2f
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-07-14 11:17:00 +0530

    change height and colors

--- mindmate-frontend/src/components/button/CustomButton.jsx
+++ mindmate-frontend/src/components/button/CustomButton.jsx
@@ -15,61 +15,62 @@ const ButtonComponent = styled.button`
   border-radius: ${(props) => props.borderRadius}px;
   width: ${(props) => props.width}px;
   padding: ${(props) =>
-      props.size === "sm"
-          ? "1.5rem"
-          : props.size === "md"
-              ? "1.8rem"
-              : props.size === "lg"
-                  ? "2.0rem"
-                  : "1.5rem"};
+          props.size === "sm"
+                  ? "1.5rem"
+                  : props.size === "md"
+                          ? "1.8rem"
+                          : props.size === "lg"
+                                  ? "2.0rem"
+                                  : "1.5rem"};
   height: ${(props) =>
-   props.size === "sm"
-      ? "34px"
-      : props.size === "md"
-      ? "37px"
-      : props.size === "lg"
-      ? "40px"
-      : "30px"};
+          props.size === "sm"
+                  ? "34px"
+                  : props.size === "md"
+                          ? "37px"
+                          : props.size === "lg"
+                                  ? "40px"
+                                          : "30px"};
 
   font-family: "Inner", "Lato";
   font-weight: 500;
   border: 1px solid transparent;
   background-color: ${(props) =>
-      props.variant === "light"
-          ? "#f8f9fa"
-          : props.variant === "dark"
-              ? "#212529"
-              : props.variant === "primary"
-                  ? "#1ce8b5"
-                  : props.variant === "cancel"
-                      ? "#758188"
-                      : props.variant === "update"
-                          ? "#6f66df"
-                          : props.variant === "edit"
-                              ? "#006de4"
-                              : props.variant === "warning"
-                                  ? "#ffc107"
-                                  : props.variant === "danger"
-                                      ? "#dc3545"
-                                      : "#f8f9fa"};
+          props.variant === "light"
+                  ? "#f8f9fa"
+                  : props.variant === "dark"
+                          ? "#212529"
+                          : props.variant === "primary"
+                                  ? "#1e5d88"
+                                  : props.variant === "cancel"
+                                          ? "#758188"
+                                          : props.variant === "update"
+                                                  ? "#6f66df"
+                                                  : props.variant === "edit"
+                                                          ? "#006de4"
+                                                          : props.variant === "warning"
+                                                                  ? "#ffc107"
+                                                                  : props.variant === "danger"
+                                                                          ? "#dc3545"
+                                                                          : "#f8f9fa"};
   color: ${(props) =>
-      props.variant === "light"
-          ? "#000000"
-          : props.variant === "dark"
-              ? "#ffffff"
-              : props.variant === "primary"
-                  ? "#ffffff"
-                  : props.variant === "cancel"
-                      ? "#ffffff"
-                      : props.variant === "success"
+          props.variant === "light"
+                  ? "#000000"
+                  : props.variant === "dark"
                           ? "#ffffff"
-                          : props.variant === "info"
-                              ? "#ffffff"
-                              : props.variant === "warning"
+                          : props.variant === "primary"
                                   ? "#ffffff"
-                                  : props.variant === "danger"
-                                      ? "#ffffff"
-                                      : "#ffffff"};
+                                  : props.variant === "cancel"
+                                          ? "#ffffff"
+                                          : props.variant === "success"
+                                                  ? "#ffffff"
+                                                  : props.variant === "info"
+                                                          ? "#ffffff"
+                                                          : props.variant === "warning"
+                                                                  ? "#ffffff"
+                                                                  : props.variant === "danger"
+                                                                          ? "#ffffff"
+                                                                          : "#ffffff"};
+
   &:hover,
   &:focus {
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
@@ -78,36 +79,36 @@ const ButtonComponent = styled.button`
 
 `;
 const CustomButton = ({
-  type,
-  variant,
-  className,
-  id,
-  onclick,
-  children,
-  width,
-  radius,
-  size,
-  fontSize,
-    height,
-}) => {
-  return (
-    <ButtonComponent
-      type={type ? type : "button, file"}
-      variant={variant}
-      className={
-        className ? `button-components ${className}` : "btn-components"
-      }
-      id={id}
-      onClick={onclick}
-      width={width}
-      borderRadius={radius}
-      size={size}
-      fontSize={fontSize}
-      height={height}
-    >
-      {children}
-    </ButtonComponent>
-  );
+                          type,
+                          variant,
+                          className,
+                          id,
+                          onclick,
+                          children,
+                          width,
+                          radius,
+                          size,
+                          fontSize,
+                          height,
+                      }) => {
+    return (
+        <ButtonComponent
+            type={type ? type : "button, file"}
+            variant={variant}
+            className={
+                className ? `button-components ${className}` : "btn-components"
+            }
+            id={id}
+            onClick={onclick}
+            width={width}
+            borderRadius={radius}
+            size={size}
+            fontSize={fontSize}
+            height={height}
+        >
+            {children}
+        </ButtonComponent>
+    );
 };
 
 
