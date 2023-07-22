66cea9d25e39da7b338e3319bab8038c88f8fccd
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-07-22 22:31:30 +0530

    change color

--- mindmate-frontend/src/view/welcomePage/Welcome.jsx
+++ mindmate-frontend/src/view/welcomePage/Welcome.jsx
@@ -1,31 +1,31 @@
 import logo from "../../assets/logo/Logo.png";
 import robot from "../../assets/robotPicture/robot.png";
 import CustomButton from "../../components/button/CustomButton";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./Welcome.css";
 
 const Welcome = (props) => {
-  return (
-    <div className="welcome-main">
-      <img src={logo} alt="MindMate" className="maidMate-logo" />
-      <p className="welcome">Welcome Back</p>
-      <img src={robot} alt="Robot" className="robot" />
-      <br />
-      <Link to="/signIn">
-        <CustomButton
-          type="button"
-          variant="light"
-          radius="20"
-          size="sm"
-          className="mt-5"
-          fontSize="20"
-          width="200"
-        >
-          Get Started
-        </CustomButton>
-      </Link>
-    </div>
-  );
+    return (
+        <div className="welcome-main">
+            <img src={logo} alt="MindMate" className="maidMate-logo"/>
+            <p className="welcome">Welcome Back</p>
+            <img src={robot} alt="Robot" className="robot"/>
+            <br/>
+            <Link to="/signIn">
+                <CustomButton
+                    type="button"
+                    variant="light"
+                    radius="20"
+                    size="sm"
+                    className="mt-5"
+                    fontSize="20"
+                    width="200"
+                >
+                    Get Started
+                </CustomButton>
+            </Link>
+        </div>
+    );
 };
 
 export default Welcome;
