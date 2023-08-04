dd8308d976dfa001d7f33e135aa2707996bcc342
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:28:52 +0530

    Created controller for 'child'

---
+++ mindmate-backend/src/main/java/com/uwu/emora/controller/ChildController.java
@@ -0,0 +1,22 @@
+package com.uwu.emora.controller;
+
+import com.uwu.emora.service.ChildService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/child")
+public class ChildController {
+
+    private final ChildService childService;
+
+    //http://localhost:8080/child/test
+    @GetMapping(value = "/test")
+    public ResponseEntity test() {
+        return ResponseEntity.ok("Hello Test");
+    }
+}
