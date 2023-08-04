8db3e98530c78db1c657c1277cc79abd9bc2a987
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:22:17 +0530

    Created repository for 'child emotion'

---
+++ mindmate-backend/src/main/java/com/uwu/emora/repository/ChildEmotionRepository.java
@@ -0,0 +1,9 @@
+package com.uwu.emora.repository;
+
+import com.uwu.emora.entity.ChildEmotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChildEmotionRepository extends JpaRepository<ChildEmotion, Long> {
+}
