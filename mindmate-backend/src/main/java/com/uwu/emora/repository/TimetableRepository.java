9d1bde5f384df5a0baf44d6810ed4ea497c93025
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:23:05 +0530

    Created repository for 'timetable'

---
+++ mindmate-backend/src/main/java/com/uwu/emora/repository/TimetableRepository.java
@@ -0,0 +1,9 @@
+package com.uwu.emora.repository;
+
+import com.uwu.emora.entity.Timetable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimetableRepository extends JpaRepository<Timetable, Long> {
+}
