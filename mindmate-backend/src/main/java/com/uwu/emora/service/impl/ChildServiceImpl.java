2848854a6a955602cae17137431300db989440c9
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:31:14 +0530

    Updated child service implementation

--- mindmate-backend/src/main/java/com/uwu/emora/service/impl/ChildServiceImpl.java
+++ mindmate-backend/src/main/java/com/uwu/emora/service/impl/ChildServiceImpl.java
@@ -1,9 +1,10 @@
 package com.uwu.emora.service.impl;
 
+import com.uwu.emora.service.ChildService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ChildServiceImpl {
+public class ChildServiceImpl implements ChildService {
 }
