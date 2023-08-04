2e34fb40d57c02b2fc43782d0a666f9637bb9924
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:29:21 +0530

    Created service implementation for 'child'

---
+++ mindmate-backend/src/main/java/com/uwu/emora/service/impl/ChildServiceImpl.java
@@ -0,0 +1,9 @@
+package com.uwu.emora.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChildServiceImpl {
+}
