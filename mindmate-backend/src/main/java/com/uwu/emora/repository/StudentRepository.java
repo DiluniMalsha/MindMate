9792121213398ce9c480d78d52004e4db095fd52
DiluniMalsha <diluni.malsha.peiris@gmail.com>
2023-08-04 15:28:37 +0530

    Deleted unnecessary classes

--- mindmate-backend/src/main/java/com/uwu/emora/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uwu.emora.repository;
-
-import com.uwu.emora.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
