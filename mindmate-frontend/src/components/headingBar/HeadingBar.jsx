978b14f2dc08c06490706f88cdadf6b3e1ca505d
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-08-06 00:58:07 +0530

    added heading center title and css

--- mindmate-frontend/src/components/headingBar/HeadingBar.jsx
+++ mindmate-frontend/src/components/headingBar/HeadingBar.jsx
@@ -4,16 +4,29 @@ import Grid from "@mui/material/Grid";
 import SetDate from "../homeHeading/SetDate";
 import WishPerTime from "../homeHeading/WishPerTime";
 import React from "react";
-
+import robotAnimate from "../../assets/robotPicture/animate-robot.gif"
 const HeadingBar = (props) => {
     return (
         <div>
             <Grid container spacing={0}>
-                <Grid item xs={11}>
+                <Grid item xs={9} md={10} xl={11}>
                     <div className = 'heading-bar'>
                         <div className = 'row'>
                             <div className="col">
                                 <SetDate/>
+                            </div>
+                            <div className = 'col ms-5 display-set'>
+                                <div className="row">
+                                    <div className='col-4'>
+                                       <div className='Emora-name'>
+                                        Emora
+                                    </div>
+                                    </div>
+                                    <div className='col-8 text-sm-start'>
+                                     <img src={robotAnimate} alt='Animate Robot' width='120px' height='80px'/>
+                                        <br/>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col">
                                 <WishPerTime/>
@@ -21,7 +34,7 @@ const HeadingBar = (props) => {
                         </div>
                     </div>
                 </Grid>
-                <Grid item xs={1}>
+                <Grid item xs={3} md={2} xl={1}>
                     <img src={pic} alt="ProfilePic" className='profile-pic-img'/>
                 </Grid>
             </Grid>
