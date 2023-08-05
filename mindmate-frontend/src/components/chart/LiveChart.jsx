a8314319403fcf499cc79529ebafaa8cad488d81
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-08-05 11:39:46 +0530

    live chat added css

--- mindmate-frontend/src/components/chart/LiveChart.jsx
+++ mindmate-frontend/src/components/chart/LiveChart.jsx
@@ -1,18 +1,17 @@
+import "./LiveChart.css";
 import {LineChart} from '@mui/x-charts/LineChart';
-import "./LiveChart.css"
-import * as React from "react";
 import CustomButton from "../button/CustomButton";
 import Grid from "@mui/material/Grid";
 
-const LiveChart = ({width, height}) => {
+const LiveChart = ({title, width, height, display}) => {
     return (
         <>
             <div className="checking-pattern-btn-set">
                 <p className="title-align btn-title">
-                    Her Past Emotion Changing Pattern
+                    {title}
                 </p>
                 <Grid container spacing={2} sx={{marginBottom:'20px'}}>
-                    <Grid item xs={6} md={8} sx={{textAlign:'left'}}>
+                    <Grid item xs={12} md={8} sx={{textAlign:'left'}} className='grid-btn-set'>
                         <CustomButton
                             type="button"
                             variant="active"
@@ -22,6 +21,7 @@ const LiveChart = ({width, height}) => {
                             className="chart-btn-responsive"
                             fontSize="18"
                             width="150"
+                            display = {display}
                         >
                             Day
                         </CustomButton>
@@ -33,6 +33,7 @@ const LiveChart = ({width, height}) => {
                             className="chart-btn-responsive"
                             fontSize="18"
                             width="150"
+                            display = {display}
                         >
                             3 Days
                         </CustomButton>
@@ -44,28 +45,30 @@ const LiveChart = ({width, height}) => {
                             className="chart-btn-responsive"
                             fontSize="18"
                             width="150"
+                            display = {display}
                         >
                             Week
                         </CustomButton>
                     </Grid>
-                    <Grid item xs={6} md={4} sx={{textAlign:'right'}}>
+                    <Grid item xs={12} md={4} sx={{textAlign:'right'}} className='history-grid'>
                         <CustomButton
                             type="button"
                             variant="active"
                             radius="20"
                             size="sm"
-                            className="chart-btn-responsive align-right me-0"
+                            className="chart-btn-responsive history-btn-style display-his-btn me-0"
                             fontSize="18"
                             width="180"
+                            display = {display}
                         >
                             Check History
                         </CustomButton>
                     </Grid>
                 </Grid>
                 {/*</div>*/}
-
+                {/*sx={{border: '1px solid #1E5D88'}}*/}
                 <div className="chart-border">
-                    <LineChart sx={{border: '1px solid #1E5D88'}}
+                    <LineChart
                                xAxis={[{data: [1, 2, 3, 5, 8, 10]}]}
                                series={[
                                    {
@@ -76,6 +79,19 @@ const LiveChart = ({width, height}) => {
                                height={height}
                     />
                 </div>
+                <Grid item xs={12} md={4} sx={{textAlign:'right'}} className='history-grid'>
+                    <CustomButton
+                        type="button"
+                        variant="active"
+                        radius="20"
+                        size="sm"
+                        className="chart-btn-responsive history-btn-style display-his-btn-2  me-0"
+                        fontSize="18"
+                        width="180"
+                    >
+                        Check History
+                    </CustomButton>
+                </Grid>
             </div>
         </>
 
