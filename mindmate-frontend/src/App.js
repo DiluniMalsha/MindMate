a7c4ec3ddd1edf08f33b47bf9418e92dfdccc922
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-08-05 11:41:14 +0530

    remove routers in app.js

--- mindmate-frontend/src/App.js
+++ mindmate-frontend/src/App.js
@@ -1,49 +1,13 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Settings from "./view/settings/Settings";
-import PageNotFound from "./components/pageNotFound/PageNotFound";
-import SignIn from "./view/SignIn/SignIn";
-import NavigationPanel from "./components/navigationPanels/NavigationPanel";
-import Scheduler from "./view/scheduler/Scheduler";
-import Home from "./view/home/Home";
-import Grid from "@mui/material/Grid";
-import NavigationPanelMobile from "./components/navigationPanels/NavigationPanelMobile";
-import ReminderCalender from "./components/reminderCalender/ReminderCalender";
-import './App.css'
-import EmotionTracker from "./view/EmotionTracker/EmotionTracker";
+import AppRouter from "./router/AppRouter";
 
-const App = (props) => {
+const App = () => {
     // const token = true;
 
     return (
 
-        <div className="App" style={{ height:'100vh'}}>
-            <BrowserRouter>
+        <div className="App" style={{height: '100vh'}}>
 
-                <Grid container>
-                    <Grid item sm={1} xs={0} className='background-color'>
-                            <div className='NavDesktopDisplay'>
-                                <NavigationPanel/>
-                            </div>
-                        <div className='NavMobileDisplay'>
-                            <NavigationPanelMobile/>
-                        </div>
-                    </Grid>
-                    <Grid item sm={11} xs={12} sx={{textAlign:'center', justifyContent:'center'}}>
-                        <div className="center-div">
-                            <Routes>
-                                <Route path='/*' element={<PageNotFound/>}/>
-                                {/*<Route path='/' element={<Welcome/>}/>*/}
-                                <Route path="/signIn" element={<SignIn/>}/>
-                                <Route path="/settings" element={<Settings/>}/>
-                                <Route path="/scheduler" element={<Scheduler/>}/>
-                                <Route path="/home" element={<Home/>}/>
-                                <Route path="/reminder" element={<ReminderCalender/>}/>
-                                <Route path="/emotion-tracker" element={<EmotionTracker/>}/>
-                            </Routes>
-                        </div>
-                    </Grid>
-                </Grid>
-            </BrowserRouter>
+            <AppRouter/>
         </div>
     );
 }
