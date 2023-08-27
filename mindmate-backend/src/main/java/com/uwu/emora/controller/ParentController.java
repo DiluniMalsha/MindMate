751beee663a12379a945b89c5dbc7eae78df0006
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-08-27 16:02:12 +0530

    add coresOrigin

--- mindmate-backend/src/main/java/com/uwu/emora/controller/ParentController.java
+++ mindmate-backend/src/main/java/com/uwu/emora/controller/ParentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/parent")
+@CrossOrigin
 public class ParentController {
 
     private final ParentService parentService;
