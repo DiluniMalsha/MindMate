50476258f962bbdcdaf2bc4f80d3c8cc8d3fb5a9
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-09-17 14:41:35 +0530

    set base url

--- mindmate-frontend/src/utils/repository.js
+++ mindmate-frontend/src/utils/repository.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-// import {BASE_URL} from "./constants";
+import {BASE_URL} from "./constants";
 
 const accessToken: string | null = localStorage.getItem("loggedUserToken");
 const Repository = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: BASE_URL, //"http://localhost:8080/api"
     headers:{
         'Content-Type': 'application/json',
         'Accept': 'application/json',
