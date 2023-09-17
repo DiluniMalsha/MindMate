71efa7dad201e4f94dcb1c85cf85b23de79b6182
Sanidu-Udawatta <73054986+Sanidu-Udawatta@users.noreply.github.com>
2023-09-17 14:42:23 +0530

    scheduler function added

--- mindmate-frontend/src/repository/schedulerRepository.js
+++ mindmate-frontend/src/repository/schedulerRepository.js
@@ -8,7 +8,22 @@ const deleteScheduledTasks = (childId, reminderId) => {
     return Repository.delete('/scheduler/onetime/' + childId + '/' + reminderId)
 }
 
+const updateSchedulerTask = (childId, schedulerDetails) => {
+    return Repository.put('/scheduler/onetime/' + childId, schedulerDetails)
+}
+
+const addSchedulerTask = (childId, schedulerDetails) => {
+    return Repository.post('/scheduler/onetime/' + childId, schedulerDetails)
+}
+
+const getUpcomingTask = (childId) => {
+    return Repository.get('/scheduler/upcoming/'+ childId)
+}
+
 export {
     getScheduledTasks,
-    deleteScheduledTasks
+    deleteScheduledTasks,
+    updateSchedulerTask,
+    addSchedulerTask,
+    getUpcomingTask
 }
